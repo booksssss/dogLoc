c4b8fa2c13eba1ab86c5780b8d7395f5201c8fb3
booksssss <86739545+booksssss@users.noreply.github.com>
2022-08-23 18:11:13 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Screens/Login';
+import Dashboard from './Screens/Dashboard';
+import {createSwitchNavigator, createAppContainer} from 'react-navigation'
+import firebase from 'firebase'
+import {firebaseConfig} from './config'
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig)
+}else{
+  firebase.app()
+}
+
+const AppSwitchNavigator = createSwitchNavigator({
+
+  LoginScreen:LoginScreen,
+  Dashboard:Dashboard 
+
+})
+const AppNavigator = createAppContainer(AppSwitchNavigator)
+export default function App() {
+  return (
+   <AppNavigator/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
